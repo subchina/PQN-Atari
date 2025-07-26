f10bdd7565905f66285be0d8faff5d73c4d84a2c
subchina <131053435+subchina@users.noreply.github.com>
2025-07-26 04:22:28 +0200

    Update get_plots.py

--- code/get_plots.py
+++ code/get_plots.py
@@ -1,3 +1,5 @@
+" Replace ??? with corresponding learning and target environment (e.g. Space Invaders and Breakout)"
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,15 +10,15 @@ def plot_from_logged_returns(csv_path):
     x = df["Step"]
 
     # Base
-    base_mean = df["Group: Space Invaders Base - test/returned_episode_returns"]
-    base_min = df["Group: Space Invaders Base - test/returned_episode_returns__MIN"]
-    base_max = df["Group: Space Invaders Base - test/returned_episode_returns__MAX"]
+    base_mean = df["Group: ??? Base - test/returned_episode_returns"]
+    base_min = df["Group: ??? Base - test/returned_episode_returns__MIN"]
+    base_max = df["Group: ??? Base - test/returned_episode_returns__MAX"]
     base_std = (base_max - base_min) / 2
 
     # Transfer
-    transfer_mean = df["Group: Space Invaders Transfer from Demon Attack - test/returned_episode_returns"]
-    transfer_min = df["Group: Space Invaders Transfer from Demon Attack - test/returned_episode_returns__MIN"]
-    transfer_max = df["Group: Space Invaders Transfer from Demon Attack - test/returned_episode_returns__MAX"]
+    transfer_mean = df["Group: ??? Transfer from ??? - test/returned_episode_returns"]
+    transfer_min = df["Group: ??? Transfer from ??? - test/returned_episode_returns__MIN"]
+    transfer_max = df["Group: ??? Transfer from ??? - test/returned_episode_returns__MAX"]
     transfer_std = (transfer_max - transfer_min) / 2
 
     # Plot
@@ -30,10 +32,10 @@ def plot_from_logged_returns(csv_path):
 
     plt.xlabel("Environment Steps")
     plt.ylabel("Episode Return")
-    plt.title("Space Invaders: Baseline vs Transfer from Demon Attack; Greedy Policy")
+    plt.title("???: Baseline vs Transfer from ???")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
     plt.show()
 
-plot_from_logged_returns("wandb_export_2025-07-26T03_35_48.526+02_00 sit.csv")
+plot_from_logged_returns(".csv") # CSV name
